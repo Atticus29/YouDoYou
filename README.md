45c1e888e45d9b5e41c2362d1b7c97b36b414d55
slaughtr <dslaughtr@gmail.com>
2017-04-10 10:16:54 -0700

    Basic project creation, boilerplate

---
+++ README.md
@@ -0,0 +1,29 @@
+# _YouDoYou_
+
+#### _YouDoYou, 04-10-2017_
+
+#### By _**asdf and asdf**_
+
+## Description
+_Example text for the description of the project_
+
+
+## Specifications
+
+| Behavior                   | Input Example     | Output Example    |
+| -------------------------- | -----------------:| -----------------:|
+
+
+
+## Setup/Installation Requirements
+
+* _Clone the repository_
+* _Run the command 'gradle run'_
+* _Open browser and go to localhost:4567_
+
+
+### License
+
+Copyright (c) 2017 **_asdf and asdf_**
+
+This software is licensed under the MIT license.
---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,26 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/you_do_you_test", null, null);
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteAvatarQuery = "DELETE FROM avatars *;";
+      con.createQuery(deleteAvatarQuery).executeUpdate();
+      String deleteUserQuery = "DELETE FROM users *;";
+      con.createQuery(deleteUserQuery).executeUpdate();
+      String deleteSkillQuery = "DELETE FROM skills *;";
+      con.createQuery(deleteSkillQuery).executeUpdate();
+      String deleteTaskListQuery = "DELETE FROM task_lists *;";
+      con.createQuery(deleteTaskListQuery).executeUpdate();
+      String deleteTaskQuery = "DELETE FROM tasks *;";
+      con.createQuery(deleteTaskQuery).executeUpdate();
+    }
+  }
+}
