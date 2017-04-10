7c5b1651532754b55ba1ee78a649dd88dae7f3d7
Mark <mark.aaron.fisher@gmail.com>
2017-04-10 16:26:55 -0700

    pass taskList constructor

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -8,11 +8,29 @@ import java.util.TimerTask;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
-public class TaskList {
+public class TaskList extends TodoAbstract {
   private int number_tasks;
 
-  public TaskList() {
-    
+  public TaskList(String name, int priority_level, Timestamp due, int skill_id, int user_id) {
+    this.name = name;
+    this.priority_level = priority_level;
+    this.due = due;
+    this.completed = false;
+    this.skill_id = skill_id;
+    this.user_id = user_id;
+    // number_tasks will be incremented upon association of task with tasklist (i.e., the task constructor) TODO
+  }
+
+  public int getNumber_tasks(){
+    return this.number_tasks;
+  }
+
+  public void markCompleted(){
+    this.completed = true;
+  }
+
+  public void setNumber_tasks(int number){
+    this.number_tasks = number;
   }
 
 }
