b9de7d4115bd67331694e35df7d9b1d8f545a916
Mark <mark.aaron.fisher@gmail.com>
2017-04-13 12:01:32 -0700

    capture a snapshot of a footer display

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,7 +18,7 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("users", User.all());
+      model.put("user", dummy);
       model.put("template", "templates/index.vtl");
       model.put("tasks", Task.all());
       model.put("tasklists", TaskList.all());
@@ -28,7 +28,7 @@ public class App {
 
     get("/tasks", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("users", User.all());
+      model.put("user", dummy);
       model.put("template", "templates/tasks.vtl");
       model.put("tasks", Task.all());
       model.put("tasklists", TaskList.all());
@@ -38,7 +38,7 @@ public class App {
 
     get("/tasks/:id", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("users", User.all());
+      model.put("user", dummy);
       Task task = Task.find(Integer.parseInt(request.params("id")));
       model.put("task", task);
       model.put("template", "templates/task.vtl");
@@ -50,7 +50,7 @@ public class App {
 
     get("/tasklists", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("users", User.all());
+      model.put("user", dummy);
       model.put("template", "templates/tasklists.vtl");
       model.put("tasks", Task.all());
       model.put("tasklists", TaskList.all());
@@ -60,7 +60,7 @@ public class App {
 
     get("/tasklists/:id", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("users", User.all());
+      model.put("user", dummy);
       TaskList tasklist = TaskList.find(Integer.parseInt(request.params("id")));
       model.put("tasklist", tasklist);
       model.put("template", "templates/task.vtl");
@@ -72,7 +72,7 @@ public class App {
 
     get("/skills", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("users", User.all());
+      model.put("user", dummy);
       model.put("template", "templates/skills.vtl");
       model.put("tasks", Task.all());
       model.put("tasklists", TaskList.all());
@@ -82,7 +82,7 @@ public class App {
 
     get("/skills/:id", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("users", User.all());
+      model.put("user", dummy);
       Skill skill = Skill.findSkill(Integer.parseInt(request.params("id")));
       model.put("skill", skill);
       model.put("template", "templates/skill.vtl");
@@ -94,7 +94,7 @@ public class App {
 
     get("/player/:playerName", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("users", User.all());
+      model.put("user", dummy);
       String currentUserName = request.params(":playerName");
       User currentUser = User.findUserByName(currentUserName);
       model.put("player", currentUser);
